02650a75bec792239002909f0ec3b4848da079b8
lanzhengxian <lanzhengxian@gmail.com>
2016-10-12 16:11:51 +0800

    add signup page + react-router

--- client/components/Greetings.js
+++ client/components/Greetings.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
-export default () => {
+export default class Greetings extends React.Component {
+  render(){
   return (
-    <h1>Hi !</h1>
-  );
+    <div className="jumbotron">
+    <h1>Hi!</h1>
+    </div>
+  );}
 }
